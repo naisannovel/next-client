31c39a51635a86ec60af4f6df7aaeac7e1c8fde0
naisannovel <naisannovel@gmail.com>
2021-10-04 23:04:03 +0600

    update

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 
 const Hero = () => {
+    const history = useHistory();
     return (
         <div className='container hero__container'>
             <div className="row hero__row__container">
                 <div className="col-md-6 hero__content">
                     <h1>Next is a place to write, read, and connect</h1>
                     <p>It's easy and free to post your thinking on any topic and connect with millions of readers.</p>
-                    <button className='primary__btn'>Start Writing</button>
+                    <button onClick={()=>history.push('/add/post')} className='primary__btn'>Start Writing</button>
                 </div>
                 <div className="col-md-6">
                     <img src="assets/images/hero.svg" alt="hero" />
