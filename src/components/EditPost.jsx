e8fa333344e29d0f41d7708277f8a44ba2ef59f7
naisannovel <naisannovel@gmail.com>
2021-10-04 18:48:16 +0600

    update

--- src/components/EditPost.jsx
+++ src/components/EditPost.jsx
@@ -1,50 +1,58 @@
-import React, { useState } from "react";
-import { Alert, Form, FormGroup } from "reactstrap";
-import { useForm } from "react-hook-form";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { API } from "../utilities/baseURL";
+import { useParams } from "react-router";
+import _ from 'lodash';
 
 const EditPost = () => {
   const [loading, setLoading] = useState(false);
-  const [addSuccessMsg, setAddSuccessMsg] = useState(null);
-  const { register, handleSubmit, reset } = useForm();
+  const [post, setPost] = useState({});
+  const [showUpdateBtn,setShowUpdateBtn] = useState(false);
 
-  const onSubmit = (data) => {
-    // setLoading(true);
-    //       axios.post(`${API}/post`,data,{
-    //         headers:{
-    //           "Authorization": `Bearer ${JSON.parse(localStorage.getItem('token'))}`
-    //         }
-    //       })
-    //       .then(res => {
-    //         setLoading(false);
-    //         setAddSuccessMsg(res.data.message);
-    //         setTimeout(()=>{
-    //           setAddSuccessMsg(null);
-    //         },2000)
-    //       })
-    //       .catch(err => console.log(err.response.message));
-    console.log(data);
-    reset();
+  const inputOnChangeHandler = event => {
+      const data = { ...post };
+      data[event.target.name] = event.target.value
+      setShowUpdateBtn(true);
+      setPost(data);
   };
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`${API}/post/${id}`, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+        },
+      })
+      .then((res) => {
+        setLoading(false);
+        setPost(_.pick(res.data,["title","body"]));
+      })
+      .catch((err) => console.log(err.response.data));
+  }, []);
 
   return (
     <div className="edit__post__container">
       <h1>Edit Post</h1>
       <hr />
       {/* { addSuccessMsg !== null && <Alert color='success' style={{fontSize:'16px'}}>{addSuccessMsg}</Alert>} */}
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <FormGroup>
-          <input type="text" {...register("title")}/>
-        </FormGroup>
-        <FormGroup>
+      <form>
+          <input
+            type="text"
+            name="title"
+            onChange={inputOnChangeHandler}
+            value={post.title}
+          />
           <textarea
             type="textarea"
-            {...register("body")}
+            name="body"
+            value={post.body}
+            onChange={inputOnChangeHandler}
           />
-        </FormGroup>
-        <button className="primary__btn">Update</button>
-      </Form>
+        {showUpdateBtn && <button type='submit' className="primary__btn">Update</button>}
+      </form>
     </div>
   );
 };
